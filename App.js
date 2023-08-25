6341b6a78ac4b4495cbf05bb272fd2e0829ad1ba
jeongkite <jeong6kite@gmail.com>
2023-08-25 19:22:06 +0900

    Create Font Style 설정

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { Fonts } from "./constants/fonts";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "sf-pro-regular": require("./assets/fonts/SF-Pro-Text-Regular.otf"),
+    "sf-pro-thin": require("./assets/fonts/SF-Pro-Text-Thin.otf"),
+    "sf-pro-medium": require("./assets/fonts/SF-Pro-Text-Medium.otf"),
+    "sf-pro-semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+    "sf-pro-bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
+  });
+
+  // if (!fontsLoaded) {
+  // }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={Fonts.bold}>Bold Text!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ constants/fonts.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const Fonts = StyleSheet.create({
+  regular: {
+    fontFamily: "sf-pro-regular",
+  },
+  bold: {
+    fontFamily: "sf-pro-bold",
+  },
+  semibold: {
+    fontFamily: "sf-pro-semibold",
+  },
+  medium: {
+    fontFamily: "sf-pro-medium",
+  },
+  thin: {
+    fontFamily: "sf-pro-thin",
+  },
+});
