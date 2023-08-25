7986a1e9ef50adb39d6f6c8466cc57ff6eeeb969
jeongkite <jeong6kite@gmail.com>
2023-08-25 19:39:22 +0900

    Feat Navigator에 Screen 등록

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
-import { Fonts } from "./constants/fonts";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import AllNft from "./screens/AllNft";
+import NftDetail from "./screens/NftDetail";
+import MintingNft from "./screens/MintingNft";
+import MyPage from "./screens/MyPage";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,15 +17,23 @@ export default function App() {
     "sf-pro-bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
   });
 
-  // if (!fontsLoaded) {
-  // }
+  // TODO: 폰트 로딩 여부에 따라 스플래시 표시하기
+
+  const Stack = createNativeStackNavigator();
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text style={Fonts.bold}>Bold Text!!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="AllNft" component={AllNft} />
+          <Stack.Screen name="NftDetail" component={NftDetail} />
+          <Stack.Screen name="MintingNft" component={MintingNft} />
+          <Stack.Screen name="MyPage" component={MyPage} />
+          <Stack.Screen name="ChargePoint" component={ChargePoint} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
---
+++ screens/ChargePoint.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+function ChargePoint() {}
+
+export default ChargePoint;
+
+const styles = StyleSheet.create({});
