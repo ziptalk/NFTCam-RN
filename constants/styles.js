8a4a7fec917c385d46e680499b047b8e5b17ff4a
jeongkite <jeong6kite@gmail.com>
2023-08-25 18:16:39 +0900

    Create styles.js

---
+++ constants/styles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    gray000: "#FFFFFF",
+    gray100: "#EEEEEE",
+    gray200: "#CDCDCD",
+    gray300: "#AEAEB2",
+    gray400: "#8E8E93",
+    gray500: "#5F5F5F",
+    gray600: "#3E3E3E",
+    gray700: "#2C2C2F",
+    gray800: "#222222",
+    gray900: "#151515",
+    primary: "#007AFF",
+    error: "#FF453A",
+  },
+};
