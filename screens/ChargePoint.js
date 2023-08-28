e217f001abac133ee5db8c54db4f830e9aa07145
jeongkite <jeong6kite@gmail.com>
2023-08-28 18:13:34 +0900

    Style Charge Point screen

--- screens/ChargePoint.js
+++ screens/ChargePoint.js
@@ -1,7 +1,40 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-function ChargePoint() {}
+import SmallIndex from "../components/UI/SmallIndex";
+import { GlobalStyles } from "../constants/styles";
+import WideButton from "../components/UI/WideButton";
+
+function ChargePoint({ route, navigation }) {
+  return (
+    <View style={styles.root}>
+      <SmallIndex>CURRENT POINT</SmallIndex>
+      <Text style={styles.currentPoint}>5,432 P</Text>
+      <SmallIndex>POINTS TO CHARGE</SmallIndex>
+      <Text style={styles.chargePoint}>100 P</Text>
+      <WideButton style={styles.chargeButton}>Charge Point</WideButton>
+    </View>
+  );
+}
 
 export default ChargePoint;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    padding: 15,
+  },
+  currentPoint: {
+    fontSize: 20,
+    fontFamily: GlobalStyles.fonts.bold,
+    color: GlobalStyles.colors.gray200,
+    marginBottom: 30,
+  },
+  chargePoint: {
+    fontSize: 32,
+    fontFamily: GlobalStyles.fonts.bold,
+    color: "white",
+  },
+  chargeButton: {
+    marginTop: 18,
+  },
+});
