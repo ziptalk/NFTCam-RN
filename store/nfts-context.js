58f4bf542a5d6a7ed7a3eba5e56f408753c679b2
jeongkite <jeong6kite@gmail.com>
2023-09-08 21:59:03 +0900

    Delete nftContext 더미데이터 삭제

--- store/nfts-context.js
+++ store/nfts-context.js
@@ -1,96 +1,5 @@
 import { createContext, useReducer } from "react";
 
-const DUMMY_NFT = [
-  {
-    materialId: 21,
-    source: "https://www.hanbit.co.kr/data/editor/20191017121027_wgbsqeit.png",
-    date: "23.12.27 PM 02:15",
-    device: "아이폰x",
-    address: "서울특별시 강남구 강남대로84길 8",
-    isMinting: "MINTED",
-  },
-  {
-    materialId: 20,
-    source: "https://www.hanbit.co.kr/data/editor/20191017121027_wgbsqeit.png",
-    date: "23.12.27 PM 02:15",
-    device: "아이폰x",
-    address: "서울특별시 강남구 강남대로84길 8",
-    isMinting: "MINTED",
-  },
-  {
-    materialId: 19,
-    source: "https://www.hanbit.co.kr/data/editor/20191017121027_wgbsqeit.png",
-    date: "23.12.27 PM 02:15",
-    device: "아이폰x",
-    address: "서울특별시 강남구 강남대로84길 8",
-    isMinting: "MINTED",
-  },
-  {
-    materialId: 18,
-    source: "https://www.hanbit.co.kr/data/editor/20191017121027_wgbsqeit.png",
-    date: "23.12.27 PM 02:15",
-    device: "아이폰x",
-    address: "서울특별시 강남구 강남대로84길 8",
-    isMinting: "MINTED",
-  },
-  {
-    materialId: 17,
-    source: "https://www.hanbit.co.kr/data/editor/20191017121027_wgbsqeit.png",
-    date: "23.12.27 PM 02:15",
-    device: "아이폰x",
-    address: "서울특별시 강남구 강남대로84길 8",
-    isMinting: "MINTED",
-  },
-  {
-    materialId: 16,
-    source: "https://www.hanbit.co.kr/data/editor/20191017121027_wgbsqeit.png",
-    date: "23.12.27 PM 02:15",
-    device: "아이폰x",
-    address: "서울특별시 강남구 강남대로84길 8",
-    isMinting: "MINTED",
-  },
-  {
-    materialId: 15,
-    source: "https://www.hanbit.co.kr/data/editor/20191017121027_wgbsqeit.png",
-    date: "23.12.27 PM 02:15",
-    device: "아이폰x",
-    address: "서울특별시 강남구 강남대로84길 8",
-    isMinting: "MINTED",
-  },
-  {
-    materialId: 14,
-    source: "https://www.hanbit.co.kr/data/editor/20191017121027_wgbsqeit.png",
-    date: "23.12.27 PM 02:15",
-    device: "아이폰x",
-    address: "서울특별시 강남구 강남대로84길 8",
-    isMinting: "MINTED",
-  },
-  {
-    materialId: 13,
-    source: "https://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg",
-    date: "23.12.27 PM 02:15",
-    device: "아이폰x",
-    address: "서울특별시 강남구 강남대로84길 8",
-    isMinting: "MINTED",
-  },
-  {
-    materialId: 12,
-    source: "https://www.hanbit.co.kr/data/editor/20191017121027_wgbsqeit.png",
-    date: "23.12.27 PM 02:15",
-    device: "아이폰x",
-    address: "서울특별시 강남구 강남대로84길 8",
-    isMinting: "MINTED",
-  },
-  {
-    materialId: 11,
-    source: "https://www.hanbit.co.kr/data/editor/20191017121027_wgbsqeit.png",
-    date: "23.12.27 PM 02:15",
-    device: "아이폰x",
-    address: "서울특별시 강남구 강남대로84길 8",
-    isMinting: "MINTED",
-  },
-];
-
 export const NftsContext = createContext({
   nfts: [],
   setNfts: () => {},
@@ -112,7 +21,7 @@ function nftReducer(state, action) {
 }
 
 function NftsContextProvider({ children }) {
-  const [nftState, dispatch] = useReducer(nftReducer, DUMMY_NFT);
+  const [nftState, dispatch] = useReducer(nftReducer);
 
   function setNfts(nfts) {
     dispatch({ type: "SET", payload: nfts });
